f6902aac3b8045f1d092219134967ff740576d65
Weslley-Prado <weslley.prado73@gmail.com>
2025-07-18 21:06:45 -0300

    create project

---
+++ src/main/java/br/com/dagostini/infrasystem/InfrasystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.dagostini.infrasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrasystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfrasystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dagostini/infrasystem/InfrasystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dagostini.infrasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrasystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
